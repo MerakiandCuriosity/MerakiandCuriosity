8d73fbfa6db3f18aad5426103b128ce175a16f86
machuan <155619408+MerakiandCuriosity@users.noreply.github.com>
2025-07-24 14:13:55 +0800

    Delete empty_LP_MSPM0G3507_nortos_ticlang directory

--- empty_LP_MSPM0G3507_nortos_ticlang/Hardware/encoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ENCODER_H
-#define _ENCODER_H
-#include "ti_msp_dl_config.h"
-
-// 编码器变量声明
-extern int32_t Get_Encoder_countA, encoderA_cnt, Get_Encoder_countB, encoderB_cnt;
-extern uint32_t gpio_interrup;
-
-#endif
